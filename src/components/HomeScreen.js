39610eeec1e46449e932de9b84d29b77aefaee7d
Edgar Chavez <edgarchavez@Edgars-MacBook-Air.local>
2023-01-19 00:21:19 -0600

    add welcome img

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
+import welcome from "../images/welcome.png";
+import "./HomeScreen.scss";
 
 function HomeScreen() {
-  return <div></div>;
+  return (
+    <div>
+      <section className="welcome-section">
+        <h1 className="welcome-section-title">SO FRESH AND SO CLEAN CLEAN!</h1>
+        <div className="welcome-section-img-container">
+          <img src={welcome} className="welcome-section-img" />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default HomeScreen;
